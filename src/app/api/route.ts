0354c4a18abd03485b1124b757be378cdc57764b
Sam Law <samuel.law@system1group.com>
2024-03-17 22:26:37 +0000

    Another potential API timestamp fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,14 @@
 import Ably from 'ably/promises';
+import { headers } from 'next/headers';
 
-export async function GET(_req: Request) {
+export async function GET(req: Request) {
+  const headersList = headers();
+  const referer = headersList.get('referer');
+
+  const clientId = referer ? new URL(referer).hostname : 'localhost';
+
   const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo', timestamp: Date.now() });
+  const tokenRequestData = await client.auth.createTokenRequest({ clientId, timestamp: Date.now() });
+  
   return Response.json(tokenRequestData);
 };
