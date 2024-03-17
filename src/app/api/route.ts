a8fd5aac56f52276f675919c71b25a355400e829
Sam Law <samuel.law@system1group.com>
2024-03-17 22:07:11 +0000

    Potential timestamp bug fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,6 +2,6 @@ import Ably from 'ably/promises';
 
 export async function GET(_req: Request) {
   const client = new Ably.Realtime(process.env.ABLY_API_KEY as string);
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
+  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo', timestamp: Date.now() });
   return Response.json(tokenRequestData);
 };
